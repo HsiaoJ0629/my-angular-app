3e5fdef3748414466ab948421c2f2d88767699e9
yuio789789 <yuio789789@gmail.com>
2025-05-14 04:43:15 +0000

    navigate fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,7 @@ import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}, // Default route
+  {path: '**', redirectTo: '/home'} // Wildcard route
 ];

9910651a1253a11a77a4a8750e246bc5c5db60a2
Arthur Hsiao <arthurh@attvest.com.au>
2025-05-15 09:00:40 +1000

    contact commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../modules/material.module';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ MaterialModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  email: string = 'arthur.hsiao0629@gmail.com';
 }

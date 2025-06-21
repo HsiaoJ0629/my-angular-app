cc0d045acb7f2117534663fa5524dfb5269289ff
Arthur Hsiao <yuio789789@gmail.com>
2025-06-21 10:26:14 +1000

    style adjusted

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,5 +8,6 @@ import { MaterialModule } from '../../modules/material.module';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  welcomeMsg: string = 'Explore My Angular App! Discover my career journey, skills, and some demos — all in one place. '
+    + 'This app is continuously updated, so check back often for the latest!';
 }
